5f386cefd7594bbbec0f7bfff23e578e0799e286
MamzelNyf <fanny@shedesign.dev>
2020-09-19 10:50:17 +0800

    create nav with router

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <nav>
+      <Link to="/" className={"button"}>
+        Home
+      </Link>
+      <Link to="/event" className={"button"}>
+        Event
+      </Link>
+    </nav>
+  );
+}
+
+export default Nav;
---
+++ src/pages/EventPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EventPage() {
+  return <h1>This is the Event Page</h1>;
+}
+
+export default EventPage;
